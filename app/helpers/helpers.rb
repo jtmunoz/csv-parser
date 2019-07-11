7d21ea7c1bc1e8883d87f32293acb1db10d8bd73
jtmunoz <jordantmunoz@gmail.com>
2019-07-10 21:39:04 -0700

    adds app directory

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,18 @@
+helpers do
+
+	def render_view(path, options={})
+		erb(path.to_sym, options)
+	end
+
+	def render_partial(path, options={})
+		parts = path.to_s.split('/')
+		filename = parts.pop
+		newpath = [parts, "_#{filename}"].join('/')
+		erb(newpath.to_sym, options.merge(:layout => false))
+	end
+
+	def h(text)
+		Rack::Utils.escape_html(text)
+	end
+
+end
