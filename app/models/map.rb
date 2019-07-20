83d2d1a7c6f903c36987d205fc9f6a16094e9fa7
jtmunoz <jordantmunoz@gmail.com>
2019-07-20 16:27:47 -0700

    updates map model, index and layout views, adds map controller

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,5 @@
 class Map < ActiveRecord::Base
   # Remember to create a migration!
+  	validates_presence_of :title, message: "Needs A Title"
+	# validates_presence_of :body, message: "Needs A Body"
 end
