17c843fc7b52150ff7c7d19fd831ab72a9a998cd
jtmunoz <jordantmunoz@gmail.com>
2019-07-21 13:21:08 -0700

    adds validation of file to map model

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,5 +1,5 @@
 class Map < ActiveRecord::Base
   # Remember to create a migration!
   	validates_presence_of :title, message: "Needs A Title"
-	# validates_presence_of :body, message: "Needs A Body"
+	validates_presence_of :file, message: "Needs A File"
 end
